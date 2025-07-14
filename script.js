650831b54ed95825d3a0b55a15c19f2943be625e
mohamadazaruddin <azaruddin1307@gmail.com>
2025-07-14 23:09:38 +0530

    download button added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const modal = document.getElementById("modal-bg");
-const downloadBtn = document.getElementById("downloadBtn");
+// const downloadBtn = document.getElementById("downloadBtn");
 const confirmBtn = document.getElementById("confirmDownload");
 const cancelBtn = document.getElementById("cancelDownload");
 
@@ -7,4 +7,14 @@ const cancelBtn = document.getElementById("cancelDownload");
 
 function cancel() {
   document.getElementById("modal-bg").style.display = "none";
+}
+function download() {
+  const link = document.createElement("a");
+  //link to the uploaded image
+  //  in your local storage
+  link.href = "application-3c7489b0-8fcf-426f-b97f-c9ba100fbbd4.aab";
+  link.download = "application-3c7489b0-8fcf-426f-b97f-c9ba100fbbd4.aab";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
 }
