4acc8b7dc2e4016f0e863d52c3015229d5d133bd
mohamadazaruddin <azaruddin1307@gmail.com>
2025-07-14 22:53:01 +0530

    download attributes added

--- script.js
+++ script.js
@@ -2,14 +2,8 @@ const modal = document.getElementById("modal-bg");
 const downloadBtn = document.getElementById("downloadBtn");
 const confirmBtn = document.getElementById("confirmDownload");
 const cancelBtn = document.getElementById("cancelDownload");
-const fileUrl = "/application-3c7489b0-8fcf-426f-b97f-c9ba100fbbd4.aab"; // Replace with your actual file URL
 
 // Hide modal initially
-
-function download() {
-  //   modal.style.display = "none";
-  window.location.href = fileUrl;
-}
 
 function cancel() {
   document.getElementById("modal-bg").style.display = "none";
